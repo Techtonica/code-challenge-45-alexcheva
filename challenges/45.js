9cc3aee95a268ed9c25093984863c33185d30973
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-09 10:07:42 -0800

    solve 45

--- challenges/45.js
+++ challenges/45.js
@@ -1,18 +1,29 @@
 // Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
-//
-// max_end3([1, 2, 3]) → [3, 3, 3]
-//
-// max_end3([11, 5, 9]) → [11, 11, 11]
-//
-// max_end3([2, 11, 3]) → [3, 3, 3]
-//
 // Bonus Challenge:  Try to do this _without_ initializing a new array.
 
 
 function max_end3(int_array){
-  // Add functionality here!
-  return
+  if(int_array[0]>int_array[2]){
+    for(let i in int_array) {
+      int_array[i] = int_array[0];
+    }
+  }else{
+    for(let i in int_array) {
+      int_array[i] = int_array[2];
+    }
+  }
+  console.log(int_array);
+  return int_array;
 }
+
+max_end3([1, 2, 3]) 
+//// [3, 3, 3]
+
+max_end3([11, 5, 9]) 
+//// [11, 11, 11]
+
+max_end3([2, 11, 3]) 
+//// [3, 3, 3]
 
 
 module.exports = max_end3;
